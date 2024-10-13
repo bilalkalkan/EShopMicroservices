b404f7a49bf79c640e57f59006360c9ac643bb26
Bilal Kalkan <thebilalkalkan@gmail.com>
2024-10-13 15:58:28 +0300

    part 5 completed

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuryHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+
+
+public interface IQuryHandler<in TQuery,TRessponse>:IRequestHandler<TQuery,TRessponse>
+    where TQuery:IQuery<TRessponse>
+    where TRessponse : notnull
+{
+}
